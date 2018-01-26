36dc52abb09e0151bd4ccc6bc001c25206257c88
Yizhao Gao <yizhaotsccsj@gmail.com>
2018-01-26 09:19:55 -0600

    add document to the code

--- src/gdalio.cpp
+++ src/gdalio.cpp
@@ -12,12 +12,33 @@
 #include <ogr_api.h>
 #include <cpl_conv.h>
 
+/**
+ * NAME:	gdalIORegister
+ * DESCRIPTION:	Register drivers
+ * PARAMETERS: void
+ */
 void gdalIORegister() 
 {	
 	GDALAllRegister();
 	OGRRegisterAll();
 }
 
+/**
+ * NAME:	getCellCenterLatLon
+ * DESCRIPTION:	Get the latitude and longtitude of pixel centers given a grid
+ * PARAMETERS:
+ * 	int outputEPSG:		EPSG code of output spatial reference system 
+ *	double xMin:		west boundary of output area
+ *	double yMin:		south boundary of output area
+ *	double xMax:		east boundary of output area
+ *	double yMax: 		north boundary of output area
+ * 	double cellSize:	output raste cell size
+ *	double ** px:		longitude of output pixel centers 
+ *	double ** py:		latitude of ouput pixel centers
+ * Output:
+ *	double ** px:		longitude of output pixel centers, memory will be allocated in this function
+ *	double ** py:		latitude of ouput pixel centers, memory will be allocated in this function
+ */
 int getCellCenterLatLon(int outputEPSG, double xMin, double yMin, double xMax, double yMax, double cellSize, double ** px, double ** py) 
 {
 
@@ -82,6 +103,19 @@ int getCellCenterLatLon(int outputEPSG, double xMin, double yMin, double xMax, d
 
 }
 
+/**
+ * NAME:	writeGeoTiff
+ * DESCRIPTION:	Write the output grid as a GeoTiff
+ * PARAMETERS:
+ *	char * fileName:	output GeoTiff file name
+ * 	double * grid:		the grid of the output radianc values
+ * 	int outputEPSG:		EPSG code of output spatial reference system 
+ *	double xMin:		west boundary of output area
+ *	double yMin:		south boundary of output area
+ *	double xMax:		east boundary of output area
+ *	double yMax: 		north boundary of output area
+ * 	double cellSize:	output raste cell size
+ */
 void writeGeoTiff(char * fileName, double * grid, int outputEPSG, double xMin, double yMin, double xMax, double yMax, double cellSize)
 {	
 	int nRow = ceil((yMax - yMin) / cellSize);
--- src/gdalio.h
+++ src/gdalio.h
@@ -1,14 +1,52 @@
 /**
  * gdalio.h
  * Authors: Yizhao Gao <ygao29@illinois.edu>
- * Date: {01/14/2018}
+ * Date: {01/25/2018}
  */
 
 #ifndef GDALIOH
 #define GDALIOH
 
+/**
+ * NAME:	gdalIORegister
+ * DESCRIPTION:	Register drivers
+ * PARAMETERS: void
+ */
 void gdalIORegister();
+
+
+/**
+ * NAME:	getCellCenterLatLon
+ * DESCRIPTION:	Get the latitude and longtitude of pixel centers given a grid
+ * PARAMETERS:
+ * 	int outputEPSG:		EPSG code of output spatial reference system 
+ *	double xMin:		west boundary of output area
+ *	double yMin:		south boundary of output area
+ *	double xMax:		east boundary of output area
+ *	double yMax: 		north boundary of output area
+ * 	double cellSize:	output raste cell size
+ *	double ** px:		longitude of output pixel centers 
+ *	double ** py:		latitude of ouput pixel centers
+ * Output:
+ *	double ** px:		longitude of output pixel centers, memory will be allocated in this function
+ *	double ** py:		latitude of ouput pixel centers, memory will be allocated in this function
+ */
 int getCellCenterLatLon(int outputEPSG, double xMin, double yMin, double xMax, double yMax, double cellSize, double ** px, double ** py);
+
+
+/**
+ * NAME:	writeGeoTiff
+ * DESCRIPTION:	Write the output grid as a GeoTiff
+ * PARAMETERS:
+ *	char * fileName:	output GeoTiff file name
+ * 	double * grid:		the grid of the output radianc values
+ * 	int outputEPSG:		EPSG code of output spatial reference system 
+ *	double xMin:		west boundary of output area
+ *	double yMin:		south boundary of output area
+ *	double xMax:		east boundary of output area
+ *	double yMax: 		north boundary of output area
+ * 	double cellSize:	output raste cell size
+ */
 void writeGeoTiff(char * fileName, double * grid, int outputEPSG, double xMin, double yMin, double xMax, double yMax, double cellSize);
 
 #endif
